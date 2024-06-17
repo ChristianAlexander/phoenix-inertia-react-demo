9be668d795892a275a4bea7a36c084cbabd70386
Christian Alexander <christian@linux.com>
2024-06-17 12:37:09 -0700

    chore(mix): install Inertia

--- mix.exs
+++ mix.exs
@@ -57,7 +57,8 @@ defmodule DemoApp.MixProject do
       {:gettext, "~> 0.20"},
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.1.1"},
-      {:bandit, "~> 1.2"}
+      {:bandit, "~> 1.2"},
+      {:inertia, "~> 0.7"}
     ]
   end
 
