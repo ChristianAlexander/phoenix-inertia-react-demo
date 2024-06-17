ead39fee66508cd4b7e07e8e956c04509a92f17a
Christian Alexander <christian@linux.com>
2024-06-17 12:40:36 -0700

    chore: configure Inertia module

--- config/config.exs
+++ config/config.exs
@@ -61,6 +61,34 @@ config :logger, :console,
 # Use Jason for JSON parsing in Phoenix
 config :phoenix, :json_library, Jason
 
+
+config :inertia,
+  # The Phoenix Endpoint module for your application. This is used for building
+  # asset URLs to compute a unique version hash to track when something has
+  # changed (and a reload is required on the frontend).
+  endpoint: DemoAppWeb.Endpoint,
+
+  # An optional list of static file paths to track for changes. You'll generally
+  # want to include any JavaScript assets that may require a page refresh when
+  # modified.
+  static_paths: ["/assets/app.js"],
+
+  # The default version string to use (if you decide not to track any static
+  # assets using the `static_paths` config). Defaults to "1".
+  default_version: "1",
+
+  # Enable server-side rendering for page responses (requires some additional setup,
+  # see instructions below). Defaults to `false`.
+  ssr: false,
+
+  # Whether to raise an exception when server-side rendering fails (only applies
+  # when SSR is enabled). Defaults to `true`.
+  #
+  # Recommended: enable in non-production environments and disable in production,
+  # so that SSR failures will not cause 500 errors (but instead will fallback to
+  # CSR).
+  raise_on_ssr_failure: true
+
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{config_env()}.exs"
